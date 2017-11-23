d65db17592227704dafa13beb33bde945563dafb
tiago Silva <holdonmax@gmail.com>
2017-11-23 00:37:15 -0200

    Implementando remoçao com auxilio do find

---
+++ crud-jpa-hibernate/src/com/algaworks/curso/main/RemovendoPrimeiroObjeto.java
@@ -0,0 +1,30 @@
+package com.algaworks.curso.main;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import com.algaworks.curso.model.Cliente;
+
+public class RemovendoPrimeiroObjeto {
+
+	public static void main(String[] args) {
+
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemploPU");
+		EntityManager em = emf.createEntityManager();
+
+		Cliente cliente = em.find(Cliente.class, 3L);
+
+		// sintaxe acima usa a classe enidade e a posiçao
+		// depois vai deletar a mesma
+		em.getTransaction().begin();// alteraçoes no banco
+
+		em.remove(cliente);
+
+		em.getTransaction().commit();
+
+		System.out.println("Objeto removido com sucesso");
+
+	}
+
+}
